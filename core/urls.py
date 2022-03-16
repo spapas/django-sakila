c008ef25ab9531049e6db510ee0c7480ca40680b
Serafeim Papastefanos <spapas@gmail.com>
2022-03-16 15:07:18 +0200

    More changes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html"))
+    path('', views.HomeView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from . import models
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        films = models.Film.objects.all()
+        ctx['films'] = films
+        return ctx
